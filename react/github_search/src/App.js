9c06c8dfc007a945e1c3498c46e6c337ea983695
himansu8 <himansusekharsahu50@gmail.com>
2023-11-28 11:34:43 +0530

    github search

--- react/github_search/src/App.js
+++ react/github_search/src/App.js
@@ -1,13 +1,47 @@
-
 import './App.css';
-import FetchGithub from './Components/Github';
+import Github from './Components/Github';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+
 
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    //fetchUsersData();
+  }, [])
+  function fetchUsersData(){
+    const url = "https://api.github.com/users";
+    axios.get(url)
+      .then((response) => {
+        console.log(response.data);
+        setUsers(response.data);
+
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
   return (
     <>
-    <FetchGithub />
-      
+      <Routes>
+        <Route path="/" element={<h1>home</h1>} />
+        <Route path="/javascript" element={<h1>javascript</h1>} />
+      </Routes>
+
+
+      {/* <h1>search</h1>
+      {users.map(ele =>{
+        return(
+          
+          <>
+          <h5>{ele.login}</h5>
+          <a href = {ele.html_url} target = "_blank">profile</a>
+          <hr/>
+          </>
+        )
+      })} */}
     </>
   );
 }
--- react/github_search/src/Components/Github.jsx
+++ react/github_search/src/Components/Github.jsx
@@ -1,40 +1,23 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Route, Routes } from "react-router-dom";
 
-function FetchGithub(){
-    const [users, setUsers] = useState([]);
 
-    useEffect(()=>{
-        const url = "https://api.github.com/users";
-        axios.get(url)
-        .then((response)=>{
-            console.log(response.data);
-            setUsers(response.data); 
-            
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
+// function Github(){
+    
 
-    },[])
-    return(
-        <>
-        {/* {users.map(ele =>{
-            return (
-                <>
-                <p>login : {ele.login}</p>
-                <a href={ele.html_url}>html</a>
-                <hr/>
-                </>
-            )
-        })} */}
+    
+//     return(
+//         <>
+//         {/* {users.map(ele =>{
+//             return (
+//                 <>
+//                 <p>login : {ele.login}</p>
+//                 <a href={ele.html_url}>html</a>
+//                 <hr/>
+//                 </>
+//             )
+//         })} */}
 
-        <Routes>
-            <Route path="/" element={<h1>home</h1>} />
-            <Route path="/javascript" element={<h1>javascript</h1>} />
-        </Routes>
-        </>
-    )
-}
-export default FetchGithub;
+        
+//         </>
+//     )
+// }
+// export default Github;
